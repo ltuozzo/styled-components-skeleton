9530c191ed279d04a08980a56520e9bbb489a26a
Lucas <lucas.tuozzo@mediamonks.com>
2021-06-20 21:05:44 -0300

    Add components folder. Add app styles.

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import { StyledMainContent } from './App.styles';
+
+export default function App(): JSX.Element {
+  return (
+    <StyledMainContent>
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+    </StyledMainContent>
+  );
+}
